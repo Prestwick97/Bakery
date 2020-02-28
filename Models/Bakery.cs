387078eafa629472cacc0b1b10b63d7f092d6d42
Prestwick97 <prestwick97@gmail.com>
2020-02-28 14:33:36 -0800

    add and refactor both bread and pastry classes. Cost property not changing through method.

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -12,17 +12,43 @@ namespace Bakery.Models {
       Cost = cost;
       Quantity = quantity;
     }
+
     public void BuyBread(int quantity, int cost)
     {
       if (quantity == 1)
       {
         cost += 5;
       }
-      for(int i = 1; i <= quantity; i++)
+      for(int i = 0; i < quantity; i++)
       {
         if (i % 3 == 0)
         {
           cost +=10;
+        }
+      }
+    }
+  }
+  public class Pastry
+  {
+    public int Cost{get; set;}
+    public int Quantity{get; set;}
+
+    public Pastry(int cost, int quantity)
+    {
+      Cost = cost;
+      Quantity = quantity;
+    }
+    public void BuyPastry(int quantity, int cost)
+    {
+      for(int i = 0; i < quantity; i++)
+      {
+        if (i % 3 == 0)
+        {
+          cost +=5;
+        }
+        else
+        {
+          cost +=2;
         }
       }
     }
