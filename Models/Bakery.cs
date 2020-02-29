3999ed7773a3f1a296a1134f22865d9c18e78553
Prestwick97 <prestwick97@gmail.com>
2020-02-28 16:29:12 -0800

    establish minimal functionality, create BuyPastry method

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 
 namespace Bakery.Models {
+
   public class Bread
   {
     public int Cost{get; set;}
@@ -62,54 +63,3 @@ namespace Bakery.Models {
     }
   }
 }
-  // public class Bread
-  // {
-  //   public int Cost{get; set;}
-  //   public int Quantity{get; set;}
-
-  //   public Bread(int cost, int quantity)
-  //   {
-  //     Cost = cost;
-  //     Quantity = quantity;
-  //   }
-
-  //   public void BuyBread(int quantity, int cost)
-  //   {
-  //     if (quantity == 1)
-  //     {
-  //       cost += 5;
-  //     }
-  //     for(int i = 0; i < quantity; i++)
-  //     {
-  //       if (i % 3 == 0)
-  //       {
-  //         cost +=10;
-  //       }
-  //     }
-  //   }
-  // }
-  // public class Pastry
-  // {
-  //   public int Cost{get; set;}
-  //   public int Quantity{get; set;}
-
-  //   public Pastry(int cost, int quantity)
-  //   {
-  //     Cost = cost;
-  //     Quantity = quantity;
-  //   }
-  //   public zBuyPastry(int quantity, int cost)
-  //   {
-  //     for(int i = 0; i < quantity; i++)
-  //     {
-  //       if (i % 3 == 0)
-  //       {
-  //         cost +=5;
-  //       }
-  //       else
-  //       {
-  //         cost +=2;
-  //       }
-  //     }
-  //   }
-  // }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,28 @@ class Program
   public static void Main()
   {
     Console.WriteLine("Welcome to Pierre's Bakery! We have a choice between bread and pastries.");
-    Console.WriteLine("At the moment, we have a special sale! Bread is only $5 a loaf, but if you buy two, you get one free!    .....   How many loafs would you like to purchase?");
-    string stringNumber = Console.ReadLine();
-    int desiredQuantity = int.Parse(stringNumber);
-    int cost = 0;
-    Bread newBread = new Bread(cost, desiredQuantity);
-    newBread.BuyBread(desiredQuantity, cost);
+    Console.WriteLine("At the moment, we have a special sale!");
+    Console.WriteLine("Bread: Only $5 a loaf, but if you buy two, you get one free!");
+    Console.WriteLine("Pastry: Only $2 a piece, but if you buy three, you get a dollar off!");
+    Console.WriteLine("Would you like to buy bread or pastry? (bread/pastry)");
+    string decision = Console.ReadLine();
+    if (decision == "bread")
+    {
+      Console.WriteLine("Wonderful! How many loaves of bread would you like? (Please enter a number quantity)");
+      string stringNumber = Console.ReadLine();
+      int desiredQuantity = int.Parse(stringNumber);
+      int cost = 0;
+      Bread newBread = new Bread(cost, desiredQuantity);
+      newBread.BuyBread(desiredQuantity, cost);
+    }
+    else
+    {
+      Console.WriteLine("Wonderful! How many pastries would you like? (Please enter a number quantity)");
+      string stringNumber = Console.ReadLine();
+      int desiredQuantity = int.Parse(stringNumber);
+      int cost = 0;
+      Pastry newPastry = new Pastry(cost, desiredQuantity);
+      newPastry.BuyPastry(desiredQuantity, cost);
+    }
   }
 }
-    // Console.WriteLine("You have a choice");
-    // string stringNumber = Console.ReadLine();
-    // int quantity = int.Parse(stringNumber);
-    // int cost = 0;
-    // Bread newBread = new Bread(cost, quantity);
-    // newBread.BuyBread(quantity, cost);
-    // Console.WriteLine(quantity);
-    // Console.WriteLine(cost);
